af1057e8db90d51a53dd9c9f67ba3c7713fec701
ValeriiaGrebneva <grebneva.lera97@gmail.com>
2025-10-31 22:18:56 -0700

    Decoding JSON request is in progress

--- main.go
+++ main.go
@@ -40,6 +40,28 @@ func handlerFunc(resp http.ResponseWriter, req *http.Request) {
 	resp.Write([]byte("OK"))
 }
 
+func handlerChirp(resp http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		// these tags indicate how the keys in the JSON should be mapped to the struct fields
+		// the struct fields must be exported (start with a capital letter) if you want them parsed
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		// an error will be thrown if the JSON is invalid or has the wrong types
+		// any missing fields will simply have their values in the struct set to their zero value
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	//continuation: if body is too long, return code 400 with an error
+	//if valid, respond with code 200
+}
+
 func main() {
 	var counter atomic.Int32
 	counter.Store(0)
@@ -50,6 +72,7 @@ func main() {
 	serveMux.HandleFunc("GET /api/healthz", handlerFunc)
 	serveMux.HandleFunc("GET /admin/metrics", apiCfg.handlerNRequests)
 	serveMux.HandleFunc("POST /admin/reset", apiCfg.handlerResetRequests)
+	serveMux.HandleFunc("POST /api/validate_chirp", handlerChirp)
 	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	serverStruct := http.Server{
 		Addr:    ":8080",
