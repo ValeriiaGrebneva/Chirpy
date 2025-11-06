f0f4fd1b1d1b128243607447ecb3f18552d6662e
ValeriiaGrebneva <grebneva.lera97@gmail.com>
2025-11-05 22:13:56 -0800

    JSON output is done. PostgreSQL database is started

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
@@ -40,6 +43,30 @@ func handlerFunc(resp http.ResponseWriter, req *http.Request) {
 	resp.Write([]byte("OK"))
 }
 
+func responseJSON(resp http.ResponseWriter, code int, response interface{}) {
+	resp.WriteHeader(code)
+	dat, err := json.Marshal(response)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		resp.WriteHeader(500)
+		return
+	}
+	resp.Header().Set("Content-Type", "application/json")
+	resp.Write(dat)
+	return
+}
+
+func CleanedBody(msg string) string {
+	splitted := strings.Split(msg, " ")
+	for i, word := range(splitted) {
+		word = strings.ToLower(word)
+		if word == "kerfuffle" || word == "sharbert" || word == "fornax" {
+			splitted[i] = "****"
+		}
+	}
+	return strings.Join(splitted, " ")
+}
+
 func handlerChirp(resp http.ResponseWriter, req *http.Request) {
 	type parameters struct {
 		// these tags indicate how the keys in the JSON should be mapped to the struct fields
@@ -54,12 +81,38 @@ func handlerChirp(resp http.ResponseWriter, req *http.Request) {
 		// an error will be thrown if the JSON is invalid or has the wrong types
 		// any missing fields will simply have their values in the struct set to their zero value
 		log.Printf("Error decoding parameters: %s", err)
-		w.WriteHeader(500)
+		type returnVals struct {
+			// the key will be the name of struct field unless you give it an explicit JSON tag
+			Error string `json:"error"`
+		}
+		respBody := returnVals{
+			Error: "Something went wrong",
+		}
+		responseJSON(resp, 500, respBody)
 		return
 	}
 
-	//continuation: if body is too long, return code 400 with an error
-	//if valid, respond with code 200
+	if len(params.Body) > 140 {
+		type returnVals struct {
+			// the key will be the name of struct field unless you give it an explicit JSON tag
+			Error string `json:"error"`
+		}
+		respBody := returnVals{
+			Error: "Chirp is too long",
+		}
+		responseJSON(resp, 400, respBody)
+		return
+	}
+
+	type returnVals struct {
+		// the key will be the name of struct field unless you give it an explicit JSON tag
+		Cleaned_body string `json:"cleaned_body"`
+	}
+	cleaned := CleanedBody(params.Body)
+	respBody := returnVals{
+		Cleaned_body: cleaned,
+	}
+	responseJSON(resp, 200, respBody)
 }
 
 func main() {
